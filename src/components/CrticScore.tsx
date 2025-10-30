3fa0e0b2a1c48f9e748215f8940e5cf7128b55d3
Brad Seidman <bseidman555@gmail.com>
2025-10-30 13:46:39 -0600

    Forgot to save color screen

--- src/components/CrticScore.tsx
+++ src/components/CrticScore.tsx
@@ -5,7 +5,12 @@ interface Props {
 }
 
 const CrticScore = ({ score }: Props) => {
-  return <Badge>{score}</Badge>;
+  let color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+  return (
+    <Badge fontSize="14px" paddingX={2} borderRadius="4px" colorScheme={color}>
+      {score}
+    </Badge>
+  );
 };
 
 export default CrticScore;
