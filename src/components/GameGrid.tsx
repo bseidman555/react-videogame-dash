5c84add13ac70ac618721eed86216628c6e8c13a
Brad Seidman <bseidman555@gmail.com>
2025-10-31 15:47:39 -0600

    Change list of genres to use static data

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,8 +3,6 @@ import useGames from "../Hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
-import { Genre } from "../Hooks/useGenres";
-import { Platform } from "../Hooks/usePlatforms";
 import { GameQuery } from "../App";
 
 interface Props {
@@ -19,9 +17,9 @@ const GameGrid = ({ gameQuery }: Props) => {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
-        spacing={3}
+        spacing={6}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
