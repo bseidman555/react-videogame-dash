d53e654ad1412fe2787da64522144bc7de639123
Brad Seidman <bseidman555@gmail.com>
2025-10-31 13:23:06 -0600

    Filter by genres

--- src/Hooks/useData.ts
+++ src/Hooks/useData.ts
@@ -1,4 +1,4 @@
-import { CanceledError } from "axios";
+import { AxiosRequestConfig, CanceledError } from "axios";
 import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
 
@@ -8,7 +8,7 @@ interface GetResponse<T> {
     results: T[];
 }
 
-const useData = <T>(endpoint: string) => {
+const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, deps?: any[]) => {
     const [data, setData] = useState<T[]>([]);
     const [error, setError] = useState("");
     const [isLoading, setIsloading] = useState(false)
@@ -17,7 +17,7 @@ const useData = <T>(endpoint: string) => {
         const controller = new AbortController();
         setIsloading(true);
         apiClient
-            .get<GetResponse<T>>(endpoint, { signal: controller.signal })
+            .get<GetResponse<T>>(endpoint, { signal: controller.signal, ...requestConfig })
             .then((res) => {
                 setData(res.data.results)
                 setIsloading(false);
@@ -29,7 +29,7 @@ const useData = <T>(endpoint: string) => {
             });
 
         return () => controller.abort();
-    }, []);
+    }, deps ? [...deps] : []);
     return { data, error, isLoading }
 
 }
